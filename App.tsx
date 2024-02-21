1a9cb7b5dcd07ee181f0d720a2d23fbb14d85299
Oscare101 <kirillfedortsev2000@gmail.com>
2024-02-21 13:32:57 +0200

    colors

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import colors from './constants/colors'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>
+        Open up App.tsx to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.bg,
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  text: {
+    fontSize: 40,
+    color: colors.text,
+  },
+})
