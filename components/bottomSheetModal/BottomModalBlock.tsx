15ef6b7193d55cbbcdd0a9fd039b7261d1b22ef9
Oscare101 <kirillfedortsev2000@gmail.com>
2024-02-21 15:00:56 +0200

    app icons

---
+++ components/bottomSheetModal/BottomModalBlock.tsx
@@ -0,0 +1,59 @@
+import {
+  Dimensions,
+  StatusBar,
+  TouchableWithoutFeedback,
+  View,
+  useColorScheme,
+} from 'react-native'
+import { BottomSheetModal } from '@gorhom/bottom-sheet'
+import colors from '../../constants/colors'
+
+const width = Dimensions.get('screen').width
+
+export default function BottomModalBlock(props: any) {
+  // const contentData: any = {
+  //   themeBlock: <ThemeBlockModal />,
+
+  // }
+
+  return (
+    <BottomSheetModal
+      // backgroundStyle={{ backgroundColor: '#00000000' }}
+      handleIndicatorStyle={{
+        backgroundColor: colors.accent,
+      }}
+      // handleComponent={() => (
+      //   <View style={{ width: 10, height: 10, backgroundColor: 'red' }}></View>
+      // )}
+      ref={props.bottomSheetModalRef}
+      snapPoints={props.snapPoints}
+      backdropComponent={({ style }) => (
+        <TouchableWithoutFeedback onPress={props.dismiss}>
+          <View
+            style={[
+              style,
+              {
+                backgroundColor: '#00000066',
+              },
+            ]}
+          >
+            <StatusBar backgroundColor={colors.white} />
+          </View>
+        </TouchableWithoutFeedback>
+      )}
+    >
+      <View
+        style={{
+          backgroundColor: colors.white,
+          flex: 1,
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'flex-start',
+          // overflow: 'hidden',
+        }}
+      >
+        {/* {contentData[props.content]} */}
+      </View>
+    </BottomSheetModal>
+  )
+}
--- screens/application/CalendarScreen.tsx
+++ screens/application/CalendarScreen.tsx
@@ -1,10 +1,34 @@
-import { Text, View } from 'react-native'
+import { Button, Text, View } from 'react-native'
 import globalStyles from '../../constants/globalStyles'
+import {
+  BottomSheetModal,
+  BottomSheetModalProvider,
+} from '@gorhom/bottom-sheet'
+import { useCallback, useMemo, useRef } from 'react'
+import BottomModalBlock from '../../components/bottomSheetModal/BottomModalBlock'
 
 export default function CalendarScreen({ navigation }: any) {
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null)
+  const snapPoints = useMemo(() => ['25%', '50%'], [])
+  const onPresentModal = useCallback(() => {
+    bottomSheetModalRef.current?.present()
+  }, [])
+  const onDismisModal = useCallback(() => {
+    bottomSheetModalRef.current?.present()
+  }, [])
+
   return (
-    <View style={globalStyles.container}>
-      <Text>Calendar</Text>
-    </View>
+    <BottomSheetModalProvider>
+      <View style={globalStyles.container}>
+        <Text>Calendar</Text>
+      </View>
+      <Button onPress={onPresentModal} title="Present Modal" color="black" />
+
+      <BottomModalBlock
+        bottomSheetModalRef={bottomSheetModalRef}
+        snapPoints={snapPoints}
+        dismiss={onDismisModal}
+      />
+    </BottomSheetModalProvider>
   )
 }
