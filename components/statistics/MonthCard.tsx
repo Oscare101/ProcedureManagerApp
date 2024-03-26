815f7ddae7dca35b88fff6c4385e22f3faa297b8
Oscare101 <kirillfedortsev2000@gmail.com>
2024-03-26 09:15:49 +0200

    statistics month card

---
+++ components/statistics/MonthCard.tsx
@@ -0,0 +1,80 @@
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native'
+import React, { memo } from 'react'
+import colors from '../../constants/colors'
+import text from '../../constants/text'
+import { Ionicons } from '@expo/vector-icons'
+
+const width = Dimensions.get('screen').width
+
+function MonthCard(props: { date: Date; setDate: any }) {
+  function OnPreviousMonth() {
+    const date = new Date(props.date)
+    date.setDate(0)
+    props.setDate(date)
+  }
+
+  function OnNextMonth() {
+    const date = new Date(props.date)
+    date.setDate(1)
+    date.setMonth(date.getMonth() + 1)
+    props.setDate(date)
+  }
+
+  return (
+    <View style={styles.card}>
+      <TouchableOpacity
+        activeOpacity={0.8}
+        onPress={OnPreviousMonth}
+        style={styles.button}
+      >
+        <Ionicons name="chevron-back" size={width * 0.07} color={colors.text} />
+      </TouchableOpacity>
+      <Text style={styles.title}>
+        {props.date.getFullYear()} {text.months[props.date.getMonth()]}
+      </Text>
+      <TouchableOpacity
+        activeOpacity={0.8}
+        onPress={OnNextMonth}
+        style={styles.button}
+      >
+        <Ionicons
+          name="chevron-forward"
+          size={width * 0.07}
+          color={colors.text}
+        />
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  card: {
+    width: '92%',
+    height: width * 0.12,
+    backgroundColor: colors.white,
+    alignSelf: 'center',
+    borderRadius: width * 0.03,
+    marginBottom: width * 0.02,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: width * 0.02,
+  },
+  title: {
+    fontSize: width * 0.06,
+    color: colors.text,
+  },
+  button: {
+    height: '100%',
+    aspectRatio: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
+export default memo(MonthCard)
--- screens/application/StatisticsScreen.tsx
+++ screens/application/StatisticsScreen.tsx
@@ -1,13 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import React, { useCallback, useState } from 'react'
 import globalStyles from '../../constants/globalStyles'
 import text from '../../constants/text'
 import Header from '../../components/application/Header'
+import colors from '../../constants/colors'
+import MonthCard from '../../components/statistics/MonthCard'
 
 export default function StatisticsScreen({ navigation }: any) {
+  const [date, setDate] = useState<Date>(new Date())
+
+  const setNewDate = useCallback(
+    (value: Date) => {
+      setDate(value)
+    },
+    [date]
+  )
+
   return (
-    <View style={globalStyles.container}>
+    <View style={[globalStyles.container, { backgroundColor: colors.bg }]}>
       <Header title={text.Statistics} action="drawer" />
+      <ScrollView style={{ flex: 1, width: '100%' }}>
+        <MonthCard date={date} setDate={setNewDate} />
+      </ScrollView>
     </View>
   )
 }
